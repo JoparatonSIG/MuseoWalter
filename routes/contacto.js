bac94b8b6a69691548b306b4c32fbfdfbb4c813e
wdrm093 <ccp191293@gmail.com>
2016-01-06 19:37:59 -0300

    hola

--- routes/contacto.js
+++ routes/contacto.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Museo Jesuitico', username: 'Walter' });
+  res.render('contacto', { title: 'Museo Jesuitico', username: 'Walter' });
 });
 
 module.exports = router;
